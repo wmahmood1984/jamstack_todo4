9f90f56778340ab5750d6522069b499fbc5178ed
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-11-14 12:28:06 +0500

    Theme ui added

---
+++ packages/www/gatsby-ssr.js
@@ -0,0 +1,5 @@
+const React = require("react")
+
+const wrapRootElement = require('./wrap-root-element')
+
+exports.wrapRootElement = wrapRootElement;
--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import {Button, Container, Flex, Heading} from 'theme-ui'
 
 const index = () => {
     return (
-        <div>
-            Hello world
-        </div>
+        <Container>
+            <Flex sx={{flexDirection : "column", padding:3}}>
+            <Heading as="h1">To do App</Heading>
+            <Button sx={{marginTop : 2, color: "black" }}
+            onClick={()=>alert("login")}
+            >Login</Button>
+            </Flex>
+            
+        </Container>
     )
 }
 
